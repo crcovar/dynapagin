ebf4047203dc41959b92281110931f2364a44f17
Charles Covar <covar1@gmail.com>
2013-10-18 10:31:36 -0400

    Fixes bug where page event wasn't always recieving int.
    There was a bug where directly setting a page would trigger the
    page event with a string passed for the page number, instead of
    the more natural and expected int. This has been fixed and now any
    page event or content callback will be pased an int value for the
    new page number

--- js/dynapagin.js
+++ js/dynapagin.js
@@ -1,25 +1,8 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2013 Charles Covar
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+/**
+ * @preserve Copyright (c) 2013 Charles Covar
+ * Released under the MIT License 
+ * https://raw.github.com/crcovar/dynapagin/master/LICENSE
+ */
 (function ($) { 
     $.fn.dynapagin = function (options, content) {
         var settings = $(this).extend({
@@ -34,7 +17,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
             var me = $(this);
 
             function setPage(page) {
-                settings.page = page;
+                settings.page = parseInt(page);
                 me.find(".dynapagin-selector li").removeClass('active disabled');
                 me.find(".dynapagin-selector li[data-page='"+settings.page+"']").addClass('active');
                 
